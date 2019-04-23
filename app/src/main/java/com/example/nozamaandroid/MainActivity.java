43fe1c3e3f7e6a99c9537a6a57828acfae12c41f
Sub-Z3r0 <andersen19@gmail.com>
2019-04-23 20:42:52 +0200

    connection to firebase
    working on it

--- app/src/main/java/com/example/nozamaandroid/MainActivity.java
+++ app/src/main/java/com/example/nozamaandroid/MainActivity.java
@@ -1,46 +1,86 @@
 package com.example.nozamaandroid;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.nozamaandroid.Models.ProductsMockData;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends ListActivity {
+import java.util.ArrayList;
 
-    int userClick = 0;
-
+public class MainActivity extends AppCompatActivity
+{
     public static String TAG = "ProductApp";
 
+    ArrayList<String> listItems = new ArrayList<String>();
+    ListView listView;
     ProductsMockData mockData;
 
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        // Needs to initialize before creating an instance (Should be onCreate)
+        FirebaseApp.initializeApp(this);
 
         this.setTitle("NozamaGo");
 
-        mockData = new ProductsMockData();
+        listView = findViewById(R.id.synchronizeProducts);
+
+       /* mockData = new ProductsMockData();
 
         String[] product;
 
         product = mockData.getProducts();
+        */
 
-        ListAdapter adapter =
-                new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1,
-                        product);
-
-        setListAdapter(adapter);
     }
+
+    
 
     public void openUserView(View view) {
-        userClick+=1;
-        Toast.makeText(this, "User view clicked " + userClick + " times.", Toast.LENGTH_SHORT).show();
+        try {
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("Kriss cake");
+            myRef.setValue("Soft chocklate");
+
+            // Read from the database
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    String value = dataSnapshot.getValue(String.class);
+                    Log.d(TAG, "Value is: " + value);
+                    listItems.add("Product : " + value);
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        } catch (Exception e) {
+            Log.d(TAG, "Exception: " + e);
+        }
     }
 }
+
+
