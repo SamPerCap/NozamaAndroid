fc5672cc7a497785a6aef2b5045a71b6c5914f6f
Sub-Z3r0 <andersen19@gmail.com>
2019-04-23 14:07:48 +0200

    mock data works

--- app/src/main/java/com/example/nozamaandroid/MainActivity.java
+++ app/src/main/java/com/example/nozamaandroid/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.nozamaandroid;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.nozamaandroid.Models.ProductsMockData;
+
+public class MainActivity extends ListActivity {
 
     int userClick = 0;
+
+    public static String TAG = "ProductApp";
+
+    ProductsMockData mockData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        this.setTitle("NozamaGo");
+
+        mockData = new ProductsMockData();
+
+        String[] product;
+
+        product = mockData.getProducts();
+
+        ListAdapter adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        product);
+
+        setListAdapter(adapter);
     }
 
     public void openUserView(View view) {
