19d1b5b373df55c53a6fc294e12320d081ab241c
Sub-Z3r0 <andersen19@gmail.com>
2019-04-24 15:45:05 +0200

    minor stuff

--- app/src/main/java/com/example/nozamaandroid/DALProducts/AddProduct.java
+++ app/src/main/java/com/example/nozamaandroid/DALProducts/AddProduct.java
@@ -1,4 +1,32 @@
 package com.example.nozamaandroid.DALProducts;
 
-public class AddProduct {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.nozamaandroid.R;
+
+public class AddProduct extends AppCompatActivity {
+
+    EditText editText, editText2;
+
+    @Override
+    protected void onCreate(Bundle saveInstance)
+    {
+        super.onCreate(saveInstance);
+        setContentView(R.layout.addproduct_detail);
+        editText = findViewById(R.id.dbName);
+        editText2 = findViewById(R.id.dbValue);
+    }
+
+    private void textData()
+    {
+
+    }
+
+    public void saveData(View view)
+    {
+
+    }
 }
