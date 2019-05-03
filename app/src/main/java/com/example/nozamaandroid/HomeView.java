3df3fccd133571c083e094d3c0ee02bd8c0e801e
Sub-Z3r0 <andersen19@gmail.com>
2019-05-03 12:28:34 +0200

    can now select image from filechooser and upload to storage, with unique id

--- app/src/main/java/com/example/nozamaandroid/HomeView.java
+++ app/src/main/java/com/example/nozamaandroid/HomeView.java
@@ -1,8 +1,10 @@
 package com.example.nozamaandroid;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -84,6 +86,7 @@ public class HomeView extends AppCompatActivity
     TextView cartCount;
     MenuItem menuItemLogin;
     MenuItem menuItemAccount;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,7 +103,7 @@ public class HomeView extends AppCompatActivity
         setupDataBase();
 
 
-
+        askPremision();
 
         getUser();
         clickOnList();
@@ -413,4 +416,46 @@ public class HomeView extends AppCompatActivity
             }
         });
     }
+
+    private void askPremision()
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+
+
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.CALL_PHONE)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.INTERNET)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+                    == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.READ_CONTACTS)
+                    == PackageManager.PERMISSION_DENIED) {
+
+                Log.d(TAG, "permission denied to CAMERA - requesting it");
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE
+                        , Manifest.permission.CAMERA
+                        , Manifest.permission.SEND_SMS
+                        , Manifest.permission.CALL_PHONE
+                        , Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        , Manifest.permission.INTERNET
+                        , Manifest.permission.ACCESS_FINE_LOCATION
+                        , Manifest.permission.ACCESS_COARSE_LOCATION
+                        , Manifest.permission.ACCESS_NETWORK_STATE
+                        , Manifest.permission.READ_CONTACTS};
+
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }}
 }
---
+++ app/src/main/java/com/example/nozamaandroid/Shared/FileChooser.java
@@ -0,0 +1,115 @@
+package com.example.nozamaandroid.Shared;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.nozamaandroid.DALUsers.AddUser;
+import com.example.nozamaandroid.R;
+import com.example.nozamaandroid.UserAccountDetails;
+
+public class FileChooser extends AppCompatActivity {
+    private static final int READ_REQUEST_CODE = 42;
+    String messageToCamara = "mKey";
+    String filePath;
+    String TAG = "FileCHooser";
+    String addContactName = "adduser";
+    String DetailActivity = "detailactivity";
+    String BEFriendKey = "selectedFriend";
+    String friendIdKey;
+
+    @Override
+    protected void onCreate(Bundle saveInstance) {
+        super.onCreate(saveInstance);
+        //messageToCamara = getString(R.string.activityClass);
+        friendIdKey = getString(R.string.friendKey);
+        performFileSearch();
+    }
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    public void performFileSearch() {
+
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        );
+        startActivityForResult(i, 42);
+    }
+
+    /*
+    get the android/emolated/0 path from media.
+     */
+    public String mf_szGetRealPathFromURI(final Context context, final Uri ac_Uri) {
+        String result = "";
+        boolean isok = false;
+
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(ac_Uri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            result = cursor.getString(column_index);
+            isok = true;
+        } finally {
+            if ( cursor != null ) {
+                cursor.close();
+            }
+        }
+
+        return isok ? result : "";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if ( requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK ) {
+            Log.d(TAG, "Request: " + RESULT_OK);
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+
+            Uri uri = null;
+            if ( data != null ) {
+                uri = data.getData();
+                filePath = mf_szGetRealPathFromURI(this, uri);
+                Log.i(TAG, "Uri: " + filePath);
+                // showImage(uri);
+
+            }
+            changeActivity();
+        } else {
+            finish();
+        }
+
+    }
+
+    public void changeActivity() {
+        Intent intent = getIntent();
+        String activity = intent.getStringExtra(messageToCamara);
+        Log.d(TAG, "onActivityResult:1 " + activity.toLowerCase() + " " + addContactName);
+        if ( activity.toLowerCase().equals(addContactName) ) {
+            Log.d(TAG, " go to: " + addContactName);
+            Intent camaraintent = new Intent(this, AddUser.class);
+            camaraintent.putExtra(messageToCamara, filePath);
+            startActivity(camaraintent);
+        } else if ( activity.toLowerCase().equals(DetailActivity) ) {
+            Log.d(TAG, " go to: " + DetailActivity);
+            Intent camaraintent = new Intent(this, UserAccountDetails.class);
+
+
+            camaraintent.putExtra(messageToCamara, filePath);
+
+            startActivity(camaraintent);
+        }
+    }
+}
